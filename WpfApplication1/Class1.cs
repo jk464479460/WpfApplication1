f1e56c964d5ff4839ea4f593028f4cc8f93351fb
jk464479460 <464479460@qq.com>
2016-03-10 14:09:10 +0800

    建立文件

---
+++ WpfApplication1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public delegate void UpdateUiTb(object sender, EventMy e);
+    class Class1
+    {
+    }
+
+    public class EventMy : EventArgs
+    {
+        public string Text { get; set; }
+    }
+    public class TestDispatcher
+    {
+        public event UpdateUiTb UpdateUiHandler;
+
+        public void Update()
+        {
+            var sender = this;
+            var para=new EventMy {Text=$"{DateTime.Now.Second}"};
+           UpdateUiHandler?.Invoke(sender,para);
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+   
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        DispatcherTimer timer =new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            this.btUn.Click += BtUn_Click;
+            timer.Interval = new TimeSpan(1000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TestDispatcher obj = new TestDispatcher();
+            obj.UpdateUiHandler += Obj_UpdateUiHandler;
+            obj.Update();
+        }
+
+        private void BtUn_Click(object sender, RoutedEventArgs e)
+        {
+           //obj.Update();
+        }
+
+        private void Obj_UpdateUiHandler(object sender, EventMy e)
+        {
+            this.tbBox.Text = e.Text;
+        }
+    }
+
+}
